19f4af99c225c2a8776ab5db1505782465fae9c8
jukJuchok <juk.juchok@yandex.ru>
2023-01-22 23:07:03 +0300

    inital commit

---
+++ src/components/feature/main/cardSides/BottomSide.svelte
@@ -0,0 +1,77 @@
+<script>
+  import Header from "../../../shared/fontSystem/Header.svelte";
+  import Text from "../../../shared/fontSystem/Text.svelte";
+</script>
+
+<div class="card">
+  <div class="steps">
+    <div class="step">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <img src="/images/1.png" />
+      <Text>1. о Буся!</Text>
+    </div>
+    <div class="step">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <img src="/images/2.png" />
+      <Text>2. привет</Text>
+    </div>
+    <div class="step">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <img src="/images/3.png" />
+      <Text>3. падади</Text>
+    </div>
+    <div class="step">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <img src="/images/4.png" />
+      <Text>3. ам!</Text>
+    </div>
+  </div>
+  <div class="header">
+    <Header>С днём рождения,</Header>
+    <Header>Буся!!11!!!!1</Header>
+  </div>
+</div>
+
+<style>
+  .card {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 58px;
+    justify-items: center;
+    align-content: center;
+
+    width: 100%;
+    height: 100%;
+    max-width: 506px;
+    max-height: 506px;
+
+    padding: 32px;
+
+    background: #eeb7bf;
+    border-radius: 8px;
+  }
+
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .steps {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 12px;
+  }
+
+  .step {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 4px;
+    justify-items: center;
+  }
+
+  img {
+    width: 100%;
+    border-radius: 4px;
+  }
+</style>
---
+++ src/components/shared/fontSystem/Header.svelte
@@ -0,0 +1,13 @@
+<h1>
+  <slot />
+</h1>
+
+<style>
+  h1 {
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 38px;
+    color: #fff1f3;
+    background: url("/images/glitter.gif");
+  }
+</style>
