4b3fb7aa7748a200a2bc1e37126263a44fcf680b
jukJuchok <juk.juchok@yandex.ru>
2023-01-22 23:51:59 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'node-fetch': 'isomorphic-fetch',
+		},
+	}
 };
 
 export default config;
